fea92e00ceb2786c9355d9dcbcbbca0a46970bf0
Nick Weiland <nickweiland@gmail.com>
2019-09-12 18:17:35 -0700

    :tada: Updates dev
    [master]

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              "@babel/transform-react-jsx",
-              "@babel/plugin-proposal-class-properties"
-            ]
+            presets: ['@babel/preset-env', '@babel/react'],
           }
         }
       }
